ed8a7a83c0169d0119cada67fafcc2103c333b3d
Angela Mitchell <anglarenmit@gmail.com>
2020-10-12 21:42:35 -0500

    Fetched planetary data and finished bonus mission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 // Write your JavaScript code here!
 window.addEventListener('load', function() {
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
+      response.json().then(function(json) {
+         const div = document.getElementById("missionTarget");
+
+         let index = Math.floor(Math.random()*json.length);
+         
+         div.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${json[index].name}</li>
+            <li>Diameter: ${json[index].diameter}</li>
+            <li>Star: ${json[index].star}</li>
+            <li>Distance from Earth: ${json[index].distance}</li>
+            <li>Number of Moons: ${json[index].moons}</li>
+         </ol>
+         <img src="${json[index].image}">
+         `;
+
+      });
+   });
+
    const form = document.querySelector('form');
 
    const pilotName = document.getElementById('pilotName');
